4edd61e9f16b108edd80e3bacc73812bb5055adf
m-onerl <monerl2525@gmail.com>
2025-12-18 12:26:38 +0100

    chore: get the all season click from page

--- scraper/src/test_scraper.py
+++ scraper/src/test_scraper.py
@@ -1,26 +1,29 @@
 
 import asyncio
 from playwright.async_api import async_playwright
+import logging
 
-
-seasons = ["PKO BP Ekstraklasa 2025/2026", "PKO BP Ekstraklasa 2024/2025"]
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level = logging.INFO,
+    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
 async def test_basic_scraper():
-    print("Starting Playwright test...")
+    logger.info("Starting Playwright test...")
     
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=False)  
         page = await browser.new_page()
 
         url = "https://www.flashscore.pl/pilka-nozna/polska/pko-bp-ekstraklasa/archiwum/"
-        print(f"Navigating to: {url}")
+        logger.info(f"Navigating to: {url}")
         
         await page.goto(url, wait_until="networkidle")
-        for season in seasons:
-            await page.click(season)
-            title = await page.title()
-            print(f"Page title: {title}")
+        await asyncio.sleep(2)
+        season_links = await page.query_selector_all(".archiveLatte__season")
+        logger.info(f"Found out {len(season_links)} seasons")
 
-        await asyncio.sleep(3)
+        
 
 
 if __name__ == "__main__":
